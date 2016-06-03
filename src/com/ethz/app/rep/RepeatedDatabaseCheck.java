71e885f12b6ed6b19667085624ca91ea76294f89
dhara <dhara@057f481d-c402-db11-8d0d-0014225fb3b7>
2016-06-03 11:49:00 +0000

    update

--- src/com/ethz/app/rep/RepeatedDatabaseCheck.java
+++ src/com/ethz/app/rep/RepeatedDatabaseCheck.java
@@ -1,8 +1,13 @@
 package com.ethz.app.rep;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
+import java.util.Base64;
 
 import org.json.JSONObject;
+import org.whispersystems.curve25519.Curve25519;
 
 import com.ethz.app.FirefoxCacheExtract;
 import com.ethz.app.TableChecker;
@@ -12,7 +17,7 @@ public class RepeatedDatabaseCheck {
 	public static byte[] ServerPublickey;
 	
 	
-	public RepeatedDatabaseCheck() throws SQLException 
+	public RepeatedDatabaseCheck() throws Exception 
 	{
 		System.out.println("Run");
 		this.doTableCheck();
@@ -36,6 +41,29 @@ public class RepeatedDatabaseCheck {
 		
 		String droplet = jObject.getString("droplet");
 		String dropletUrl = jObject.getString("url");
+		
+		String dropletStr = droplet.concat(dropletUrl);
+		
+		byte[] dropletByte = dropletStr.getBytes(StandardCharsets.UTF_8);
+		String signatureString = jObject.getString("signature");
+		byte[] signatureBytes = Base64.getUrlDecoder().decode(signatureString);
+		
+		try 
+		{
+			
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			byte[] hashtableBytes = md.digest(dropletByte);
+			boolean check = Curve25519.getInstance("best").verifySignature(RepeatedDatabaseCheck.ServerPublickey, hashtableBytes, signatureBytes);
+			
+			if(!check)
+				throw new RuntimeException("Signature verification failed");
+		} 
+		
+		catch (NoSuchAlgorithmException e) 
+		{
+			e.printStackTrace();
+		}
+		
 		
 	}
 	
