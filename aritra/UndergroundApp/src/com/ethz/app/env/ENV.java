380c33c0fc5f915d0d5b499ae8893df552581258
dhara <dhara@057f481d-c402-db11-8d0d-0014225fb3b7>
2016-07-13 11:27:46 +0000

    completed data added

--- aritra/UndergroundApp/src/com/ethz/app/env/ENV.java
+++ aritra/UndergroundApp/src/com/ethz/app/env/ENV.java
@@ -18,6 +18,8 @@ public class ENV {
 	public static final String APP_STORAGE_LOC = "APP_DATA";
 	public static final String APP_STORAGE_COMPLETED_DROPLET_FILE = "info.txt";
 	public static final String APP_STORAGE_DROPLET_URL = "dropletUrl.txt";
+	public static final String APP_STORAGE_COMPLETE_DATA = "data.txt";
+	
 	public static final String BROWSER_COMM_LINK = "comm.txt";
 	 
 	
