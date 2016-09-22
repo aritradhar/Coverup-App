f081071564910a758cd1215e3cf84fdea3cd8480
dhara <dhara@057f481d-c402-db11-8d0d-0014225fb3b7>
2016-09-22 12:33:03 +0000

    update

--- src/com/ethz/app/covert/CovertBrowserUtility.java
+++ src/com/ethz/app/covert/CovertBrowserUtility.java
@@ -12,16 +12,50 @@
 //*************************************************************************************
 package com.ethz.app.covert;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import com.ethz.app.env.ENV;
+
 /**
  * @author Aritra
  *
  */
 public class CovertBrowserUtility {
 
-	public static String[] getLocalSlice()
+	public static String[] getLocalSliceIds()
 	{
-		return null;
+		File sliceFileLoc = new File(ENV.APP_STORAGE_LOC + ENV.DELIM + ENV.APP_STORAGE_INTERACTIVE_DATA);
+		File[] sliceFiles = sliceFileLoc.listFiles();
+		String[] ret = new String[sliceFiles.length];
+		int i = 0;
+		for(File sliceDoc : sliceFiles)
+			ret[i++] = sliceDoc.getName();
+		return ret;
 		
+	}
+	
+	public static byte[] assembleSlices(long sliceId) throws IOException
+	{
+		String sliceDirLocation = ENV.APP_STORAGE_LOC + ENV.DELIM + ENV.APP_STORAGE_INTERACTIVE_DATA + ENV.DELIM + sliceId;
+		
+		File[] files = new File(sliceDirLocation).listFiles();
+		
+		byte[] ret = null;
+		int i = 0;
+		for(File file: files)
+		{
+			String fileName = file.getName().split("\\.")[0];
+			byte[] sliceData = Files.readAllBytes(file.toPath());
+			if(i == 0)
+				ret = new byte[sliceData.length * files.length];
+			int startIndex = Integer.parseInt(fileName);
+			System.arraycopy(sliceData, 0, ret, startIndex * sliceData.length, sliceData.length);
+			i++;
+		}
+		
+		return ret;
 	}
 	
 }
